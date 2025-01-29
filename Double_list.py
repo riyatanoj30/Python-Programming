1dce2ef5314700e8e657be9d441fa5a5cbc03274
Riya Mhatre <riyatmhatre@gmail.com>
2025-01-29 19:36:35 +0530

    First commit of Double_list

---
+++ Double_list.py
@@ -0,0 +1,10 @@
+#2.create 2 list with 5 five elements each conbine both the list and display it.
+# Create two lists with 5 elements each
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+
+# Combine both lists
+combined_list = list1 + list2
+
+# Display the combined list
+print(combined_list)
