d190154cbeb2f3aacf82f21f45ce7e23daa33e74
Riya Mhatre <riyatmhatre@gmail.com>
2025-01-29 19:34:12 +0530

    First commit of reverse_order

---
+++ reverse_order.py
@@ -0,0 +1,13 @@
+''' 1. create a list of 20 elements and display it in reverse order.
+twenty = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+twenty.reverse()
+print(twenty)
+'''
+# Create a list of 20 elements
+twenty = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# Reverse the list
+twenty.reverse()
+
+# Display the reversed list
+print(twenty)
