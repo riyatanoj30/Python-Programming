ee369e3923c4e9424b89bf8d51e794eccbfa6956
Riya Mhatre <riyatmhatre@gmail.com>
2025-01-29 19:39:39 +0530

    First commit of Subsitute

---
+++ Substitute.py
@@ -0,0 +1,11 @@
+#4. create a list which consist of course of mca sem 2. at index 5 substitute the course name with preplacement traning
+# Create a list of MCA Semester 2 courses
+mca_courses = ["Software Project Management", "Advanced Data Structure and Algorithms", "Advanced JavaScripts", 
+               "Introduction to Emerging Technologies", "Profile Building Communication", "Coding Tollkit", "Python Programming"]
+
+# Substitute the course at index 5 with "Preplacement Training"
+if len(mca_courses) > 5:
+    mca_courses[5] = "Preplacement Training"
+
+# Display the updated list
+print(mca_courses)
