097ee02a26c6a4d2e4af114679ab8f196ff0bc1a
Riya Mhatre <riyatmhatre@gmail.com>
2025-01-30 09:59:24 +0530

    First Commit of list_sum

---
+++ list_sum.py
@@ -0,0 +1,9 @@
+#5. create a list of 10 numbers and display the sum of the numbers.
+# Create a list of 10 numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Calculate the sum of the numbers
+total = sum(numbers)
+
+# Display the sum
+print("The sum of the numbers is:", total)
