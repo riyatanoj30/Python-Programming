20d83f2b6035cfdef0f73cf39bc2d982bca3a158
Riya Mhatre <riyatmhatre@gmail.com>
2025-01-30 10:01:29 +0530

    First Commit of vote

---
+++ Vote.py
@@ -0,0 +1,9 @@
+# check weather the person is eligible of vote or not
+# Input the age of the person
+age = int(input("Enter your age: "))
+
+# Check voting eligibility
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
