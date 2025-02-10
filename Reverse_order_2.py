253475129fd04c94589b54ed208c5cad17f217f3
Riya Mhatre <riyatmhatre@gmail.com>
2025-02-10 12:32:41 +0530

    First Commit of Reverse_order_2

---
+++ Reverse_order_2.py
@@ -0,0 +1,4 @@
+# Write python program using built-in functions to display a number is reverse order
+num = input("Enter a number: ")
+reverse_num = num[::-1]
+print(f"The reverse of {num} is {reverse_num}")
